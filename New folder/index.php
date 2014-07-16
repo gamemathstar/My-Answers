6fcccca1e3b31d91be742121e434e1e16829d608
gamemathstar <gamemathstar1@gmail.com>
2014-07-17 00:46:02 +0100

    My-Answer
    My-Answer

---
+++ New folder/index.php
@@ -0,0 +1,6 @@
+
+
+<form method="post" id="imageform" action="http://portal.abu.edu.ng/ajaximage.php" name='formupdater' enctype="multipart/form-data">
+ <input type="file" name="photoimg" id="photoimg" />
+ <input type="submit"  />
+ </form>
